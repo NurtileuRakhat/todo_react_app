f696a98f90787f2bc907f4e7c91997c8754c9f12
NurtileuRakhat <raaaaahaaat@gmail.com>
2024-12-28 18:58:42 +0500

    FINAL REACT 2025

--- src/App.js
+++ src/App.js
@@ -317,7 +317,6 @@ export default function TaskManager() {
               </Text>
             )}
 
-            {/* Add Task Button */}
             <Button
               fullWidth
               mt="md"
